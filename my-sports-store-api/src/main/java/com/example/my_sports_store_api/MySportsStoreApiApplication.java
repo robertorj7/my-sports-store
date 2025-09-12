b7af312337ec513bc26b52dd9931e38079670855
robertorj7 <robertorj7@yahoo.com>
2025-09-12 17:32:21 -0300

    initial commit

---
+++ my-sports-store-api/src/main/java/com/example/my_sports_store_api/MySportsStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_sports_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySportsStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySportsStoreApiApplication.class, args);
+	}
+
+}
---
+++ my-sports-store-api/src/test/java/com/example/my_sports_store_api/MySportsStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_sports_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySportsStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
